45198f455213d01528ec531aa8f9b9334da17857
Hassan <hchaudhary@carepredict.com>
2019-01-05 00:42:19 -0500

    idf 23

--- components/esp32/include/rom/BootloaderVrsnControl.h
+++ components/esp32/include/rom/BootloaderVrsnControl.h
@@ -10,7 +10,7 @@
 #include "esp_attr.h"
 #include "sdkconfig.h"
 
-#define MOTHER_SHIP_ESP_IDF_VRSN0	22
+#define MOTHER_SHIP_ESP_IDF_VRSN0	23
 
 #define BOOT_LOADER_VERSION	1 //__MK__: updated August 30, 2018
 
